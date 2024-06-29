80cdbdd9aee615585b4d243aaa3be25cb3981747
yongchane <202021113@sangmyung.kr>
2024-06-30 00:46:28 +0900

    #14 6/30 마이페이지 뷰 작업
    link를 통한 라우터와 에셋 작업 컴포넌트 작업 필요

---
+++ src/page/mypage/components/Community.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Community = () => {
+  return (
+    <div className="mypage-communication-list">
+      <div className="mypage-communication-list-image"> </div>
+      <div className="mypage-communication-list-content">
+        <div className="mypage-communication-list-name">리액트 스터디 </div>
+        <div className="mypage-communication-list-member">
+          참여자 : 32명 / 50명
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Community;

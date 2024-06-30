f10d36c867296fe4cfa478e7d45cfb5266883d4c
yongchane <202021113@sangmyung.kr>
2024-06-30 23:25:48 +0900

    #14 6/30 마이페치지 라우터 연동 및 컴포넌트,에셋 추가
    나머지 ui 작업 완료되면 뷰 만들고 훅을 이용한 데이터 연동 필요
    header 부분 css 작업 필요

--- src/page/mypage/components/Community.jsx
+++ src/page/mypage/components/Community.jsx
@@ -7,7 +7,7 @@ const Community = () => {
       <div className="mypage-communication-list-content">
         <div className="mypage-communication-list-name">리액트 스터디 </div>
         <div className="mypage-communication-list-member">
-          참여자 : 32명 / 50명
+          참여자 : 32명 / 50명 11
         </div>
       </div>
     </div>

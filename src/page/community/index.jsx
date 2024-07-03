03edb92b732ab099a5ba612fbf5ef0a35f4550fa
yongchane <202021113@sangmyung.kr>
2024-07-03 18:13:49 +0900

    #27 다시 해봄 가원이형이랑 머지 연습

--- src/page/community/index.jsx
+++ src/page/community/index.jsx
@@ -1,10 +1,5 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import CommunityItem from "./components/CommunityItem";
 // import "../../scss/page/_community.scss"
-import { CommunityContext } from "./components/CommunityProvider";
-import CommunityHeader from "./components/CommunityHeader";
-import Achievement from "../achievement/index";
 import CommunityMainPage from "./components/CommunityMainPage";
 
 const Community = () => {

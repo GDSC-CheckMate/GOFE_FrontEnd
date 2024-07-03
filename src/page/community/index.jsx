aabdea56726318a054872a9bcaea8913a027c7b2
yongchane <202021113@sangmyung.kr>
2024-07-03 18:05:34 +0900

    #28 커뮤니티 메뉴 뷰 통일화
    가원이 형 코드에 내가 만든 메뉴페이지 추가 작업

--- src/page/community/index.jsx
+++ src/page/community/index.jsx
@@ -1,40 +1,18 @@
-import React, { useContext } from "react"
-import { useNavigate } from "react-router-dom"
-import CommunityItem from "./components/CommunityItem"
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import CommunityItem from "./components/CommunityItem";
 // import "../../scss/page/_community.scss"
-import { CommunityContext } from "./components/CommunityProvider"
+import { CommunityContext } from "./components/CommunityProvider";
+import CommunityHeader from "./components/CommunityHeader";
+import Achievement from "../achievement/index";
+import CommunityMainPage from "./components/CommunityMainPage";
 
 const Community = () => {
-  const { groups } = useContext(CommunityContext)
-  const navigate = useNavigate()
-
-  const handleCreateGroup = () => {
-    navigate("/create-group")
-  }
-
   return (
-    <div className="community-page">
-      <div className="community-header">
-        <div className="tabs">
-          <button className="tab active">My</button>
-          <button className="tab">커뮤홈</button>
-        </div>
-      </div>
-      <div className="community-subheader">
-        <span className="subheader-title">참여중인 소모임</span>
-        <button className="create-group" onClick={handleCreateGroup}>
-          + 소모임 개설
-        </button>
-      </div>
-      <div className="group-list">
-        {groups.length > 0 ? (
-          groups.map((group) => <CommunityItem key={group.id} group={group} />)
-        ) : (
-          <p>참여중인 커뮤니티를 찾을 수 없음</p>
-        )}
-      </div>
+    <div>
+      <CommunityMainPage />
     </div>
-  )
-}
+  );
+};
 
-export default Community
+export default Community;
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,23 @@
-import React, { lazy, Suspense } from "react"
-import { createBrowserRouter } from "react-router-dom"
-import Loading from "../components/Loading"
-import Layout from "../layout/Layout"
-import CommunityProvider from "../page/community/components/CommunityProvider"
-import mypageRouter from "./mypageRouter"
+import React, { lazy, Suspense } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Loading from "../components/Loading";
+import Layout from "../layout/Layout";
+import CommunityProvider from "../page/community/components/CommunityProvider";
+import mypageRouter from "./mypageRouter";
+import communityRouter from "./communityRouter";
 
-const Main = lazy(() => import("../page/main"))
+const Main = lazy(() => import("../page/main"));
 
-const Achievement = lazy(() => import("../page/achievement"))
-const Community = lazy(() => import("../page/community"))
-const Mypage = lazy(() => import("../page/mypage"))
-const CreateGroup = lazy(() =>
-  import("../page/community/components/CreateGroup")
-)
+const Achievement = lazy(() => import("../page/achievement"));
+const Community = lazy(() => import("../page/community"));
+const Mypage = lazy(() => import("../page/mypage"));
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
       ...mypageRouter,
+      ...communityRouter,
       {
         path: "",
         element: (
@@ -53,18 +52,8 @@ const router = createBrowserRouter([
           </Suspense>
         ),
       },
-      {
-        path: "create-group",
-        element: (
-          <CommunityProvider>
-            <Suspense fallback={<Loading />}>
-              <CreateGroup />
-            </Suspense>
-          </CommunityProvider>
-        ),
-      },
     ],
   },
-])
+]);
 
-export default router
+export default router;

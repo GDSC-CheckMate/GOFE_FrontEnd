017aeac41e927fb2b4ed4f7a69cbd823712fa45e
yongchane <202021113@sangmyung.kr>
2024-07-16 23:14:42 +0900

    #45-커뮤 홈에서 더보기 (팔로우 팔로워 목록) 헤더 작업
    누를 시 밑줄 나오는 작업 추가

--- src/page/community/components/CommunityShowProfileMenu.jsx
+++ src/page/community/components/CommunityShowProfileMenu.jsx
@@ -1,30 +1,35 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const CommunityShowProfileMenu = ({ profileName }) => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleNavigate = (path) => {
     navigate(path, { state: { profileName } });
+  };
+
+  const getClassName = (path) => {
+    return location.pathname === path ? "nav-link underline" : "nav-link";
   };
 
   return (
     <div>
       <div className="community-show-profile-follow-title-menu">
         <div
-          className="community-show-profile-follow-title-menu-follow"
+          className={getClassName("/communityShowProfile")}
           onClick={() => handleNavigate("/communityShowProfile")}
         >
           팔로우
         </div>
         <div
-          className="community-show-profile-follow-title-menu-followers "
+          className={getClassName("/communityShowFollwers")}
           onClick={() => handleNavigate("/communityShowFollwers")}
         >
           팔로워
         </div>
         <div
-          className="community-show-profile-follow-title-menu-add-friends"
+          className={getClassName("/communityShowAddFreind")}
           onClick={() => handleNavigate("/communityShowAddFreind")}
         >
           친구추가

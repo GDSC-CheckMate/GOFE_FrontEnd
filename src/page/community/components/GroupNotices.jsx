0fd45ab4c0bf1491d6f7ffae5a1881c8396a33fb
wongakim-99 <202021000@sangmyung.kr>
2024-07-21 18:34:09 +0900

    #62 - 커뮤니티 상세 페이지 공지방 개발
    커뮤니티 상세 페이지 공지방 개발중

--- src/page/community/components/GroupNotices.jsx
+++ src/page/community/components/GroupNotices.jsx
@@ -1,12 +1,56 @@
-// src/page/community/components/GroupNotices.jsx
+// import React from 'react';
+// import NoticeEdit from "../../../assets/community/NoticeEdit.svg";
+
+// const GroupNotices = () => {
+//   const notices = [
+//     { title: '리액트 스터디 오프라인 일정 잡기 투표할게요~', date: '5월 6일 오후 7:00', author: '용zi찬' },
+//     { title: '리액트 스터디 오프라인 일정 잡기 투표할게요~', date: '5월 6일 오후 7:00', author: '용zi찬' },
+//     // 추가적인 공지글 항목들
+//   ];
+
+//   return (
+//     <div className="group-notices">
+//       <div className="notice-list">
+//         {notices.map((notice, index) => (
+//           <div key={index} className="notice-item">
+//             <h2>{notice.title}</h2>
+//             <p>{notice.date} {notice.author}</p>
+//           </div>
+//         ))}
+//       </div>
+//       <button className="edit-button">
+//         <img src={NoticeEdit} alt="edit" />
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default GroupNotices;
+
 
 import React from 'react';
+import NoticeEdit from "../../../assets/community/NoticeEdit.svg";
 
 const GroupNotices = () => {
+  const notices = [
+    { title: '리액트 스터디 오프라인 일정 잡기 투표할게요~', date: '5월 6일 오후 7:00', author: '용zi찬' },
+    { title: '리액트 스터디 오프라인 일정 잡기 투표할게요~', date: '5월 6일 오후 7:00', author: '용zi찬' },
+    // 추가적인 공지글 항목들
+  ];
+
   return (
-    <div>
-      <h4>공지글</h4>
-      <p>공지글 내용</p>
+    <div className="group-notices">
+      <div className="notice-list">
+        {notices.map((notice, index) => (
+          <div key={index} className="notice-item">
+            <h2>{notice.title}</h2>
+            <p>{notice.date} {notice.author}</p>
+          </div>
+        ))}
+      </div>
+        <button className="edit-button">
+          <img src={NoticeEdit} alt="edit" />
+        </button>
     </div>
   );
 };

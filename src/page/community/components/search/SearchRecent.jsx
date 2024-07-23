ce577060172a3cd7e359275674990911e2456eff
yongchane <202021113@sangmyung.kr>
2024-07-23 17:29:30 +0900

    #67 최근 검색어가 최신 5개만 뜨도록 수정

--- src/page/community/components/search/SearchRecent.jsx
+++ src/page/community/components/search/SearchRecent.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { ReactComponent as Plus } from "../../../../assets/community/PlusButton.svg";
 
 const SearchRecent = ({ keywordsArray, onRemove }) => {
+  // keywordsArray 배열의 마지막 4개 요소만 가져옵니다.
+  const recentKeywords = keywordsArray.slice(-5);
+
   return (
     <div className="community-search-view-recent-container">
       <div className="community-search-view-recent-title">최근 검색어</div>
       <div className="community-search-view-recent-keywords">
-        {keywordsArray.map((keyword, index) => (
+        {recentKeywords.map((keyword, index) => (
           <div key={index} className="community-search-view-recent-keyword">
             <div className="community-search-view-recent-keyword-content">
               {keyword}

7479cb2159d5a5c9ed43e58ea393e8edf7ad4ddf
wongakim-99 <202021000@sangmyung.kr>
2024-07-30 02:44:06 +0900

    #80 - 그룹 상세 페이지 일부 디자인 수정
    그룹 상세 페이지에 홈에서 일부 디자인 수정

--- src/page/community/components/GroupHome.jsx
+++ src/page/community/components/GroupHome.jsx
@@ -29,6 +29,11 @@ const GroupHome = () => {
     }
   }, [group]);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat('ko-KR', {year: 'numeric', month: 'long', day: 'numeric'}).format(date);
+  }
+
   return (
     <div>
       <div className="group-detail-section">
@@ -37,7 +42,9 @@ const GroupHome = () => {
       </div>
       <div className="group-detail-section">
         <h4 className="group-detail-section-title">목표 기간</h4>
-        <p>{group.startDate} ~ {new Date(new Date(group.startDate).setDate(new Date(group.startDate).getDate() + group.duration)).toLocaleDateString()} ({group.duration}일)</p>
+        <p>{formatDate(group.startDate)} ~ {formatDate(new Date(new Date(group.startDate).setDate(new Date(group.startDate).getDate() + group.duration)))} &nbsp; 
+          <span className="group-detail-section-title-goal-duration">{group.duration}일</span>
+        </p>
       </div>
       <div className="group-detail-section">
         <h4 className="group-detail-section-title">{isGoalReached ? "목표 기간을 달성했습니다!🎉" : `목표 달성일까지 ${daysLeft}일 남았어요!`}</h4>

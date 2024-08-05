1902fa90e9de7a8076bcc398da49d18de908091d
yongchane <202021113@sangmyung.kr>
2024-08-05 20:01:54 +0900

    #94 소모임 참여 부분 css 수정

--- src/page/community/components/ComMyPage.jsx
+++ src/page/community/components/ComMyPage.jsx
@@ -1,26 +1,26 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import CommunityMadeGroup from "./CommunityMadeGroup";
 import CommunityItem from "./CommunityItem";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from 'react-router-dom';
-import { fetchGroups } from '../../../redux/communitySlice';
+import { useNavigate } from "react-router-dom";
+import { fetchGroups } from "../../../redux/communitySlice";
 
 const ComMyPage = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const groups = useSelector((state) => state.community.groups);
   const groupStatus = useSelector((state) => state.community.status);
   console.log(groups);
 
   useEffect(() => {
     if (groupStatus === "idle") {
-      dispatch(fetchGroups())
+      dispatch(fetchGroups());
     }
-  }, [groupStatus, dispatch])
+  }, [groupStatus, dispatch]);
 
   const handleGroupClick = (groupId) => {
     navigate(`/community/group/${groupId}/home`);
-  }
+  };
 
   return (
     <div className="community-main-view-all">
@@ -28,12 +28,14 @@ const ComMyPage = () => {
       <div className="group-list">
         {groups.length > 0 ? (
           groups.map((group) => (
-            <div key={group.id} onClick={()=>handleGroupClick(group.id)}>
-              <CommunityItem group={group}/>
+            <div key={group.id} onClick={() => handleGroupClick(group.id)}>
+              <CommunityItem group={group} />
             </div>
           ))
         ) : (
-          <p>참여중인 커뮤니티를 찾을 수 없음</p>
+          <p className="mypage-alarm-group-list-p">
+            참여중인 커뮤니티를 찾을 수 없음
+          </p>
         )}
       </div>
     </div>

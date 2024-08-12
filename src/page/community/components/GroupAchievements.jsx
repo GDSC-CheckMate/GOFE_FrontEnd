5de87c003273d0837233a3069100f6f3c1f7de68
박준혁 <201921001@sangmyung.kr>
2024-08-13 00:46:40 +0900

    #39 소모임 성취페이지 프론트 구현
    컴포넌트 재사용을 통해 성취페이지 구현

--- src/page/community/components/GroupAchievements.jsx
+++ src/page/community/components/GroupAchievements.jsx
@@ -1,12 +1,43 @@
-// src/page/community/components/GroupAchievements.jsx
+import React from "react";
+import GroupAchievementCalendar from "../../achievement/GroupAchievement/GroupAchievementCalendar";
+import mockData from "../../achievement/mockData.json";
+import GroupAchievementHorizonLine from "../../achievement/GroupAchievement/GroupAchievementHorizonLine";
+import GroupAchievementRanking from "../../achievement/GroupAchievement/GroupAchievementRanking";
+import GoalComponent from "../../achievement/GroupAchievement/GroupAchievementGoal";
+import GroupAchievementGoalProgress from "../../achievement/GroupAchievement/GroupAchievementGoalProgress";
 
-import React from 'react';
+const { achievements } = mockData;
 
 const GroupAchievements = () => {
+  const myAchievement = 50; // 내 달성률
+  const averageAchievement = 60; // 평균 달성률
+  const importance = 80; // 중요도
+  const goalPeriod = 100; // 목표 기간
+  const achievedDays = 50; // 달성 일수
+  const usernickname = "릉딘"; // 사용자 닉네임
   return (
     <div>
-      <h4>성취</h4>
-      <p>성취 내용</p>
+      <GroupAchievementCalendar achievements={achievements} />
+      <GroupAchievementHorizonLine />
+
+      <GoalComponent
+        goalTitle="리액트 강의 다 듣기"
+        importance={importance}
+        nickname={usernickname}
+      />
+      <GroupAchievementGoalProgress
+        myAchievement={myAchievement}
+        averageAchievement={averageAchievement}
+        goalPeriod={goalPeriod}
+        achievedDays={achievedDays}
+      />
+      <GroupAchievementRanking
+        rankings={[
+          { nickname: "용zi찬", percentage: 60 },
+          { nickname: "용zi찬", percentage: 60 },
+          { nickname: "용zi찬", percentage: 60 },
+        ]}
+      />
     </div>
   );
 };

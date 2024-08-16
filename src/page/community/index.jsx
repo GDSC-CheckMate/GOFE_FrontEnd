4fcfb9984b6729c2828b15119cc082956f930882
kscold <201921339@sangmyung.kr>
2024-08-16 20:27:31 +0900

    #25 - 메인페이지 모달 생성

--- src/page/community/index.jsx
+++ src/page/community/index.jsx
@@ -1,7 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 
-import CommunityMainPage from "./components/CommunityMainPage";
-import { Outlet } from "react-router-dom";
+import CommunityMainPage from './components/CommunityMainPage';
+import { Outlet } from 'react-router-dom';
 
 const Community = () => {
   return <div></div>;
